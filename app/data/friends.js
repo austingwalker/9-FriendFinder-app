e080738b93e57352acb31f2ad4c2792172e1138e
World Wide <agwalker249@gmail.com>
2019-01-15 15:25:27 -0600

    Added content to: friends.js - home.html - survey.html - apiRoutes.js - htmlRoutes.js - server.js | app not functional

---
+++ app/data/friends.js
@@ -0,0 +1,51 @@
+
+var friends = [
+
+{
+    "name":"John",
+    "scores":[
+        3,
+        5,
+        1,
+        1,
+        4,
+        2,
+        4,
+        4,
+        3,
+        2
+      ]
+  },
+  {
+    "name":"Charlie",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+  {
+    "name":"Mallory",
+    "scores":[
+        1,
+        5,
+        3,
+        3,
+        1,
+        4,
+        1,
+        3,
+        2,
+        5
+      ]
+  },
+]
+
+module.exports = friends;
